dc4761347789cc665bab592125197d14734df4e5
glassysky <873661157@qq.com>
2016-02-15 19:42:51 +0800

    add plugin --uglify

--- App/Gruntfile.js
+++ App/Gruntfile.js
@@ -1,3 +1,11 @@
 /**
  * Created by cui on 2016/2/15.
  */
+//封装函数
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    grunt.registerTask('default', []);
+};
