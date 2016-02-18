6c2fec8cd0eb4e67434eedd6a88af586c4f15a29
glassysky <873661157@qq.com>
2016-02-19 01:23:31 +0800

    font-end dev environment version 1.0.0

---
+++ dist/assets/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by cui on 2016/2/19.
+ */
+var a = "test";
+
+console.log(1);
--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,17 @@ var gulp = require('gulp'),
     del = require('del');
 
 gulp.task('styles', function() {
-    return gulp.src('./public/stylesheets/*.scss')
-        .pipe(sass({ style: 'expended' }))
+    return sass( 'public/stylesheets/**/*.scss', { sourcemap: true })
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('dist/assets/css'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(gulp.dest('./public/stylesheets/dist/assets/css'))
+        .pipe(gulp.dest('dist/assets/css'))
         .pipe(notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('./public/javascripts/**/*.js')
+    return gulp.src('public/javascripts/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
@@ -39,7 +38,7 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('images', function() {
-    return gulp.src('./public/images/**/*')
+    return gulp.src('public/images/**/*')
         .pipe(imagemin(cache({ optimizationLevel: 3, progressive: true, interlaced: true })))
         .pipe(gulp.dest('dist/assets/img'))
         .pipe(notify({ message: 'Images task complete' }));
@@ -49,17 +48,17 @@ gulp.task('clean', function(cb) {
     del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
 });
 
-gulp.task('deault', ['clean'], function() {
+gulp.task('default', ['clean'], function() {
     gulp.start('styles', 'scripts', 'images');
 });
 
 gulp.task('watch', function() {
-    livereload.listen();
 
     gulp.watch('public/stylesheets/**/*.scss', ['styles']);
     gulp.watch('public/javascripts/**/*.js', ['scripts']);
     gulp.watch('public/stylesheets/**/*', ['images'])
 
+    livereload.listen();
     gulp.watch(['dist/**']).on('change', livereload.changed);
 });
 
