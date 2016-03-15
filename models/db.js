a5d469401976e9236a9d6466309a6bb52f2de8d7
glassysky <873661157@qq.com>
2016-03-16 00:07:06 +0800

    repo clear

--- models/db.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by cui on 2016/2/17.
- */
-var setting = require('../settings'),
-    Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = Require('mongodb').Server;
-
-module.exports = new Db(settings.db, new Server(setting.host, settings.port),{safe: true});
--- settings.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by cui on 2016/2/17.
- */
-module.exports = {
-    cookieSecret: 'myblog',
-    db: 'myBlog',
-    host: 'localhost',
-    port: 27017
-};
