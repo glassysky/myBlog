e154cfd422facda3e4415cc7f0a50947e4b1a57d
glassysky <873661157@qq.com>
2016-03-21 14:08:07 +0800

    add init pages

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: CONFIG.title });
 });
 
+router.get('/application', function(req, res, next) {
+  res.render('application', { title: CONFIG.title });
+});
+
+router.get('/')
+
 module.exports = router;
