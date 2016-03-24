f112688a980e07ffae03f559e017956dd29612c6
glassysky <873661157@qq.com>
2016-03-24 20:44:20 +0800

    add appshow page and route

--- routes/application.js
+++ routes/application.js
@@ -7,10 +7,28 @@ var router = express.Router();
 var CONFIG = require('../public/conf/config');
 var GLOBAL = require('../models/global');
 
-/* GET home page. */
+//根目录
 
 router.get('/', function(req, res, next) {
-    res.render('./application/index', { apps: GLOBAL.getAppsInfo() });
+    console.log(req.baseUrl);
+    var apps = GLOBAL.getAppsInfo();
+
+    apps.map(function(item){
+        item.src = req.baseUrl + "/appShow/" + item.index;
+    });
+    res.render('./application/index', { apps: apps });
+});
+
+
+//应用展示
+
+router.get('/appShow/:index', function(req, res, next) {
+    // console.log(req.params.index);
+    // console.log(req.baseUrl);
+    // console.log(req.index);
+    res.render('./application/appShow', {
+        appIndex: req.params.index
+    });
 });
 
 module.exports = router;
